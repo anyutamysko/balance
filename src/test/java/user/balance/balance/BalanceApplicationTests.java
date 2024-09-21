7351c1b4bd3868ee606c27943d75585a57c3f75f
a.mysko <anna.mysko@dxcportal.onmicrosoft.com>
2024-09-21 14:56:37 +0300

    Initial commit

---
+++ src/test/java/user/balance/balance/BalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package user.balance.balance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
